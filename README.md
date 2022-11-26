595fdd92811d567498a653b718514346d8ca0b13
kees- <6820950+kees-@users.noreply.github.com>
2022-11-25 17:05:13 -0800

    Console enhancement and output redesign

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-# substack-to-pdf
+# substack-to-json
+
+## Note
+The original script created an EPUB. This fork uses the same techniques but is intended to be an intermediate processor in the face of lacking a substack API. It skips the final rendering and outputs all blog post data to a JSON file specified in [main.py](main.py) `OUTFILE_NAME`.
+
 ## Usage:
 ```sh
 pip install -r requirements
 python main.py [Substack_URL]
 ```
 Substack_URL must point to the main page eg. [https://forcoloredgirlswhotech.substack.com/]()
-
-## Actually substack-to-epub, but:
-```sh
-sudo apt-get install pandoc
-sudo apt-get install texlive-latex-base texlive-latex-extra
-pandoc -o out.pdf input.epub
-```
-For conversion to .mobi use `calibre`.
-
-
-## TODO
- * Download assets like images
- * Cleanup code
--- main.py
+++ main.py
@@ -2,34 +2,76 @@ import os
 import sys
 import time
 import re
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pprint import pprint
-from ebooklib import epub
+# from ebooklib import epub
 
-POST_RETRY_LIMIT = 5
-FILTER = None #FILTER = "^\s*#" # Must be a vaiil regexp
+POST_RETRY_LIMIT = 3
+TIMEOUT_SECONDS = 10
+FILTER = None # FILTER = "^\s*#" # Must be a valid regexp
 ALLOW_PAYWALLED = True
 PAYWALLED_ONLY = False
 EMAIL = os.environ.get("SUBSTACK_EMAIL")
 PASSWORD = os.environ.get("SUBSTACK_PASS")
+OUTFILE_NAME = "posts.json"
+CHROME_BINARY = "/Applications/Chromium.app/Contents/MacOS/Chromium"
+SKIP_EXISTING = True
+
+try:
+  with open(OUTFILE_NAME, "r") as f:
+    agg = json.load(f)
+    existing = [ v['url'] for v in agg ]
+    f.close
+except FileNotFoundError:
+  agg = []
+  existing = []
 
 options = webdriver.ChromeOptions();
 options.add_argument('--headless');
-options.add_argument('log-level=1')
+options.add_argument('log-level=1');
+options.binary_location = CHROME_BINARY
 driver = webdriver.Chrome(options=options)
+
+# Source: https://stackoverflow.com/a/16464305
+class AnyEC:
+  """ Use with WebDriverWait to combine expected_conditions
+  in an OR.
+  """
+  def __init__(self, *args):
+      self.ecs = args
+  def __call__(self, driver):
+      for fn in self.ecs:
+          try:
+              res = fn(driver)
+              if res:
+                  return res
+          except:
+            pass
+
+def print_separator():
+  print("=" * os.get_terminal_size().columns)
+
+def write_json(file, data):
+  with open(file, 'w') as f:
+    f.write(
+      json.dumps(data, indent=2)
+    )
+    f.close
 
 def get_filename(s):
     s = str(s).strip().replace(' ', '_')
     return re.sub(r'(?u)[^-\w.]', '', s)
 
-def parse_archive(url, limit=-1, filter=None):
+def parse_archive(url, limit=None, filter=None):
     driver.get(url + '/archive?sort=new')
-    blog_name = driver.find_element(By.XPATH, '//*[@class="topbar"]//*[@class="headline"]//span[@class="name"]').text
+    blog_name = driver.find_element(By.XPATH, '//a[@class="navbar-title-link"]').text
     last_height = driver.execute_script("return document.body.scrollHeight")
+    print("Scrolling down:")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(0.5)
@@ -37,9 +79,8 @@ def parse_archive(url, limit=-1, filter=None):
             EC.invisibility_of_element_located((By.CLASS_NAME, "post-preview-silhouette"))
         )
         recent_height = driver.execute_script("return document.body.scrollHeight")
-        print(f"scrolling screen down. last:{last_height}, " 
-              f"now:{recent_height}")
-        if recent_height  == last_height:
+        print(f"  | Previous: {last_height}, Current: {recent_height}")
+        if recent_height == last_height: # TEMP (or recent_height > 5000)
             break
         last_height = recent_height
 
@@ -56,14 +97,17 @@ def parse_archive(url, limit=-1, filter=None):
         except NoSuchElementException:
             paywalled = False
         posts_parsed.append({'url': url, 'paywalled': paywalled, 'title': title})
-    pprint(posts_parsed)
+    # pprint(posts_parsed)
     return {'blog_name': blog_name, 'posts': posts_parsed}
 
 def parse_post(url):
-    print(f'parsing {url}')
+    print(f'Parsing {url}')
     driver.get(url)
-    post = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "single-post")) # d.find_element(By.CLASS_NAME, "single-post")
+    post = WebDriverWait(driver, TIMEOUT_SECONDS).until(
+        AnyEC(          
+            EC.presence_of_element_located((By.CLASS_NAME, "single-post")),
+            EC.presence_of_element_located((By.CLASS_NAME, "comments-page"))
+        )
     )
 
     try:
@@ -76,24 +120,29 @@ def parse_post(url):
     try:
         subtitle = post.find_element(By.CLASS_NAME, "subtitle").text
     except NoSuchElementException:
-        print('This post has no subtittle')
         subtitle = ""
-
-    datetime = post.find_element(By.CLASS_NAME, "post-date").get_attribute('title')
-    like_count = post.find_element(By.CLASS_NAME, "like-count").text
-    body = post.find_element(By.CLASS_NAME, "available-content").find_element(By.CLASS_NAME, "body")
+    datetime = post.find_element(By.TAG_NAME, "time").get_attribute('datetime')
+    try:
+        like_count = post.find_element(By.XPATH, '//a[contains(@class, "post-ufi-button") and contains(@class, "has-label")]//div[@class="label"]').text
+    except NoSuchElementException:
+        like_count = 0
+    body = post.find_element(By.CLASS_NAME, "body")
     text_list = [
         e.get_attribute('outerHTML') 
         for e 
         in body.find_elements(By.XPATH, './*[not(contains(@class,"subscribe-widget"))]')
     ]
     text_html = '\n'.join(text_list)
-    # pprint((post, title, subtitle, datetime, like_count, body))
-    print(f'title: {title}, paywalled: {paywalled}, likes: {like_count}')
-    return {'title': title, 'subtitle': subtitle, 'date': datetime,
-            'like_count': like_count, 'text_html': text_html,
-            'paywalled': paywalled
-           }
+    print(f'  | title:     {title[:50]}{["", "(...)"][len(title) > 40]}\n  | paywalled: {paywalled}\n  | likes:     {like_count}')
+    return {
+      'url': url,
+      'title': title,
+      'subtitle': subtitle,
+      'date': datetime,
+      'like_count': like_count,
+      'text_html': text_html,
+      'paywalled': paywalled
+    }
 
 def sign_in(email, password=None, login_link=None):
     print(f"signing for email: {email}")
@@ -107,25 +156,37 @@ def sign_in(email, password=None, login_link=None):
     )
     print("signed in")
 
-
-if __name__ == "__main__":
+def main():
     if EMAIL and PASSWORD:
         sign_in(EMAIL, PASSWORD)
-    archive = parse_archive(sys.argv[1], limit=-1, filter=FILTER)
+    archive = parse_archive(sys.argv[1], limit=None, filter=FILTER)
 
-    book = epub.EpubBook()
-    book.set_identifier('id00000')
-    book.set_title(archive['blog_name'])
-    book.set_language('en')
-    book.add_metadata('DC', 'description', 'generated by pkonkol/substack-to-pdf')
-    book.add_metadata('DC', 'publisher', 'substack-to-pdf')
+    # book = epub.EpubBook()
+    # book.set_identifier('id00000')
+    # book.set_title(archive['blog_name'])
+    # book.set_language('en')
+    # book.add_metadata('DC', 'description', 'generated by pkonkol/substack-to-pdf')
+    # book.add_metadata('DC', 'publisher', 'substack-to-pdf')
 
-    print(len(archive['posts']))
-
-    toc = []
-    spine = []
+    # toc = []
+    # spine = []
     not_posts = []
-    for i, post in enumerate(archive['posts'][::-1]):
+
+    print_separator()
+    print(f"Total posts found: {len(archive['posts'])}")
+
+    # Remove objects with urls that match a preexisting outfile.
+    # Subsequent process will skip and not update posts scraped previously.
+    if SKIP_EXISTING:
+        archive.update(
+            {'posts': list(filter(lambda d: d['url'] not in existing, archive['posts']))}
+        )
+        print(f"Number of posts already saved: {len(agg)}")
+ 
+    print_separator()
+    print(f"Posts needed to parse: {len(archive['posts'])}")
+
+    for i, post in enumerate(archive['posts'][::-1]): # TEMP ([::-1] / [:3])
         j = 0
         while True:
             try:
@@ -135,36 +196,51 @@ if __name__ == "__main__":
             except TimeoutException:
                 j += 1
                 if j >= POST_RETRY_LIMIT:
+                    p = None
                     break
-                print(f'retrying parsing {post["url"]} for {j} time')
-        if not p:
+                print(f'  Retrying parse ({j + 1} of {POST_RETRY_LIMIT} times)')
+        if p is None:
             not_posts.append(post["url"])
             continue
 
-        chapter = epub.EpubHtml(
-            title=p['title'],
-            file_name = str(i) + '.' + get_filename(p['title']) + '.xhtml',
-            lang='en'
-        )
-        chapter.content = (
-            f'<h1>{p["title"]}</h1>\n'
-             '<p>'
-            f'<time datetime={p["date"]}> {p["date"]} </time>'
-            f'<span>Likes:{p["like_count"]} </span><span> Paywalled: {p["paywalled"]}</span>'
-             '</p>\n'
-            f'<a href="{post["url"]}">URL: {post["url"]}</a>\n'
-            f'<h2>{p["subtitle"]}</h2>\n'
-        )
-        chapter.content += p['text_html']
-        book.add_item(chapter)
-        spine.append(chapter)
-        toc.append(epub.Link(str(i) + '.' + get_filename(p['title']) + '.xhtml', p['title'], ""))
-    pprint(f'not posts: {not_posts}')
+        agg.append(p)
+        write_json(OUTFILE_NAME, agg)
+        
+        continue # TEMP
 
-    book.toc = toc
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
-    book.spine = (['nav',] + spine)
-    epub.write_epub(get_filename(archive['blog_name']) + '.epub', book, {})
+        # chapter = epub.EpubHtml(
+        #     title=p['title'],
+        #     file_name = str(i) + '.' + get_filename(p['title']) + '.xhtml',
+        #     lang='en'
+        # )
+        # chapter.content = (
+        #     f'<h1>{p["title"]}</h1>\n'
+        #      '<p>'
+        #     f'<time datetime={p["date"]}> {p["date"]} </time>'
+        #     f'<span>Likes:{p["like_count"]} </span><span> Paywalled: {p["paywalled"]}</span>'
+        #      '</p>\n'
+        #     f'<a href="{post["url"]}">URL: {post["url"]}</a>\n'
+        #     f'<h2>{p["subtitle"]}</h2>\n'
+        # )
+        # chapter.content += p['text_html']
+        # book.add_item(chapter)
+        # spine.append(chapter)
+        # toc.append(epub.Link(str(i) + '.' + get_filename(p['title']) + '.xhtml', p['title'], ""))
+
+    print_separator()
+    print(f'Number of new posts skipped: {len(not_posts)}')
+
+    # book.toc = toc
+    # book.add_item(epub.EpubNcx())
+    # book.add_item(epub.EpubNav())
+    # book.spine = (['nav',] + spine)
+    # epub.write_epub(get_filename(archive['blog_name']) + '.epub', book, {})
+
+    agg.sort(key=lambda a: a['date'])
+    write_json(OUTFILE_NAME, agg)
+    print(f"Sorted and wrote any new data to {OUTFILE_NAME}")
 
     driver.quit()
+
+if __name__ == "__main__":
+    main()
